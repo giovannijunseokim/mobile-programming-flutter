f1970c0ea024a8d19a3b1520728344efa664f8b6
giovannijunseokim <kingjunseo@gmail.com>
2024-05-01 09:34:48 +0900

    5/1 ~SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -8,38 +8,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Hello Flutter"),
-        backgroundColor: Colors.amber,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello Flutter"),
+          backgroundColor: Colors.amber,
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 50,
+                color: Colors.red,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.green,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: 150,
+                height: 50,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            width: 50,
-            height: 50,
-            color: Colors.red,
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          Container(
-            width: 50,
-            height: 200,
-            color: Colors.green,
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          Container(
-            width: 50,
-            height: 50,
-            color: Colors.blue,
-          ),
-        ],
-      ),
-    ));
+    );
   }
 }
