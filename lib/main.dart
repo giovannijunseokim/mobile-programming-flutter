b9ddd2dc063bf213f332516bc9eb98d13aa7c375
giovannijunseokim <kingjunseo@gmail.com>
2024-05-01 10:06:43 +0900

    5/1 Top

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,41 @@ class MyApp extends StatelessWidget {
               height: 44,
             ),
             // Top
-            Container(),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 6, 15, 0),
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset("assets/Instagram_logo.png"),
+                  Row(
+                    children: [
+                      Image.asset(
+                        "assets/Add.png",
+                        width: 28,
+                        height: 28,
+                      ),
+                      const SizedBox(
+                        width: 18,
+                      ),
+                      Image.asset(
+                        "assets/Heart.png",
+                        width: 28,
+                        height: 28,
+                      ),
+                      const SizedBox(
+                        width: 18,
+                      ),
+                      Image.asset(
+                        "assets/Share.png",
+                        width: 28,
+                        height: 28,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
             // Body
             Expanded(
               child: Column(),
