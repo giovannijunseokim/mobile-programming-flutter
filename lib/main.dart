c9f1398b25f80f029d16775ba5635eb3924c38af
giovannijunseokim <kingjunseo@gmail.com>
2024-05-01 10:05:56 +0900

    5/1 Instagram Clone Coding Init

--- lib/main.dart
+++ lib/main.dart
@@ -9,38 +9,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello Flutter"),
-          backgroundColor: Colors.amber,
-        ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 200,
-                height: 50,
-                color: Colors.red,
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Container(
-                width: 200,
-                height: 200,
-                color: Colors.green,
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Container(
-                width: 150,
-                height: 50,
-                color: Colors.blue,
-              ),
-            ],
-          ),
+        body: Column(
+          children: [
+            // Status Bar
+            const SizedBox(),
+            // Top
+            Container(),
+            // Body
+            Expanded(
+              child: Column(),
+            ),
+            // Bottom
+            Container()
+          ],
         ),
       ),
     );
