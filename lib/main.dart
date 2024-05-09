f56c215c584c6f1aadbe7673ab4e0b085892577b
giovannijunseokim <kingjunseo@gmail.com>
2024-05-09 16:48:46 +0900

    5/8 use Stack

--- lib/main.dart
+++ lib/main.dart
@@ -16,44 +16,11 @@ class MyApp extends StatelessWidget {
               height: 44,
             ),
             // Top
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 6, 15, 0),
-              height: 50,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset("assets/Instagram_logo.png"),
-                  Row(
-                    children: [
-                      Image.asset(
-                        "assets/Add.png",
-                        width: 28,
-                        height: 28,
-                      ),
-                      const SizedBox(
-                        width: 18,
-                      ),
-                      Image.asset(
-                        "assets/Heart.png",
-                        width: 28,
-                        height: 28,
-                      ),
-                      const SizedBox(
-                        width: 18,
-                      ),
-                      Image.asset(
-                        "assets/Share.png",
-                        width: 28,
-                        height: 28,
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
+            const TopWidget(),
             // Body
             Expanded(
+                child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
               child: Column(
                 children: [
                   // user list
@@ -180,30 +147,144 @@ class MyApp extends StatelessWidget {
                       // Details
                       Container(
                         padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text.rich(
+                              TextSpan(
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                                children: [
+                                  // Number of likes
+                                  TextSpan(
+                                    text: 'awesome_application',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: '님외',
+                                  ),
+                                  TextSpan(
+                                    text: '10명',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: '이 좋아합니다',
+                                  ),
+                                ],
+                              ),
+                            ),
+                            // Empty space
+                            SizedBox(
+                              height: 8,
+                            ),
+                            // Article
+                            Text.rich(
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              TextSpan(
+                                style: TextStyle(
+                                  fontSize: 12,
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: 'flutter_framework',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                      text:
+                                          " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"),
+                                ],
+                              ),
+                            ),
+                            // Empty space
+                            SizedBox(
+                              height: 5,
+                            ),
+                            // Number of comment
+                            Text(
+                              "댓글 102개 모두 보기",
+                              style: TextStyle(
+                                  color: Colors.black54, fontSize: 12),
+                            ),
+                            // Empty space
+                            SizedBox(
+                              height: 5,
+                            ),
+                            // Last comment time
+                            Text(
+                              "1일전",
+                              style: TextStyle(
+                                color: Colors.black54,
+                                fontSize: 10,
+                              ),
+                            ),
+                            // Empty space
+                            SizedBox(
+                              height: 16,
+                            ),
+                          ],
+                        ),
                       )
                     ],
                   )
                 ],
               ),
-            ),
+            )),
             // Bottom
-            Container(
-              height: 50,
-              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset("assets/Home.png"),
-                  Image.asset("assets/Search.png"),
-                  Image.asset("assets/Reels.png"),
-                  Image.asset("assets/Shop.png"),
-                  Image.asset("assets/Profile_picture_01.png"),
-                ],
-              ),
-            )
+            const BottomWidget()
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TopWidget extends StatelessWidget {
+  const TopWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(10, 6, 15, 0),
+      height: 50,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset("assets/Instagram_logo.png"),
+          Row(
+            children: [
+              Image.asset(
+                "assets/Add.png",
+                width: 28,
+                height: 28,
+              ),
+              const SizedBox(
+                width: 18,
+              ),
+              Image.asset(
+                "assets/Heart.png",
+                width: 28,
+                height: 28,
+              ),
+              const SizedBox(
+                width: 18,
+              ),
+              Image.asset(
+                "assets/Share.png",
+                width: 28,
+                height: 28,
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
@@ -218,58 +299,113 @@ class UserProfileCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bool isFirst =
+        context.findAncestorWidgetOfExactType<Row>()?.children.first == this;
+    const LinearGradient instaGradient = LinearGradient(
+      begin: Alignment.bottomLeft,
+      end: Alignment.topRight,
+      stops: [
+        0.2,
+        0.4,
+        0.6,
+        0.8,
+        1,
+      ],
+      colors: [
+        Color(0xFFFFD600),
+        Color(0xFFFF7A00),
+        Color(0xFFFF0069),
+        Color(0xFFD300C5),
+        Color(0xFF7638FA),
+      ],
+    );
+
+    Widget addBadge = Positioned(
+      right: 2,
+      bottom: 2,
+      child: Container(
+        width: 21,
+        height: 21,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          shape: BoxShape.circle,
+          border: Border.all(
+              color: Colors.white,
+              width: 2.5,
+              strokeAlign: BorderSide.strokeAlignCenter),
+        ),
+        child: const Icon(
+          Icons.add_rounded,
+          color: Colors.white,
+          size: 13,
+          weight: 100,
+        ),
+      ),
+    );
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 6),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            width: 71,
-            height: 71,
-            padding: const EdgeInsets.all(2.5),
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
-                begin: Alignment.bottomLeft,
-                end: Alignment.topRight,
-                stops: [
-                  0.2,
-                  0.4,
-                  0.6,
-                  0.8,
-                  1,
-                ],
-                colors: [
-                  Color(0xFFFFD600),
-                  Color(0xFFFF7A00),
-                  Color(0xFFFF0069),
-                  Color(0xFFD300C5),
-                  Color(0xFF7638FA),
-                ],
-              ),
-            ),
-            child: Container(
-              padding: const EdgeInsets.all(2.5),
-              decoration: const BoxDecoration(
-                  color: Colors.white, shape: BoxShape.circle),
-              child: Container(
+          Stack(
+            children: [
+              // Insta Gradient Outer line
+              Container(
+                width: 71,
+                height: 71,
+                padding: const EdgeInsets.all(2.5),
                 decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage(profileImagePath),
+                    shape: BoxShape.circle,
+                    gradient: isFirst ? null : instaGradient),
+                child: Container(
+                  padding: const EdgeInsets.all(2.5),
+                  decoration: const BoxDecoration(
+                      color: Colors.white, shape: BoxShape.circle),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage(profileImagePath),
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
+              isFirst ? addBadge : const SizedBox.shrink()
+            ],
           ),
           SizedBox(
             width: 71,
             child: Text(
-              userName,
+              isFirst ? "내 스토리" : userName,
               textAlign: TextAlign.center,
               overflow: TextOverflow.ellipsis,
             ),
           )
+        ],
+      ),
+    );
+  }
+}
+
+class BottomWidget extends StatelessWidget {
+  const BottomWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset("assets/Home.png"),
+          Image.asset("assets/Search.png"),
+          Image.asset("assets/Reels.png"),
+          Image.asset("assets/Shop.png"),
+          Image.asset("assets/Profile_picture_01.png"),
         ],
       ),
     );
